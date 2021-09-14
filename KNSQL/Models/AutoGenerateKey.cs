3604ceed7da459a7e8168071559f6a4bc5ad86b2
Quynhchi0802 <dochi9997@gmail.com>
2021-09-14 14:12:02 +0700

    KNQ

--- KNSQL/Models/AutoGenerateKey.cs
+++ KNSQL/Models/AutoGenerateKey.cs
@@ -10,13 +10,7 @@ namespace KNSQL.Models
         public string GenerateKey(string id)
         {
             string strkey = "";
-            //kiem tra gia tri id truyen vao la rong hay khong
-            //neu id khong null
-            //tach rieng phan so va phan chu cua tham so id
-            //giu nguyen phan chu
-            //phan so chuyen doi sang kieu so nguyen va tang len 1 don vi
-            //ghep phan so voi phan chu de duoc ma tu sinh
-            //tra ve ma sau khi tu sinh
+          
             return strkey;
         }
     }
